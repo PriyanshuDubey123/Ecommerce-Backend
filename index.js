01c52ccd60c4762a6ec1a6c5e83485fa2bb43a51
PriyanshuDubey123 <priyanshudube397@gmail.com>
2024-01-09 04:56:04 +0530

    Testing

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 
 server.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount ,orderId} = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -182,6 +182,9 @@ server.post("/create-payment-intent", async (req, res) => {
     automatic_payment_methods: {
       enabled: true,
     },
+    metadata:{
+      orderId
+    }
   });
 
   res.send({
