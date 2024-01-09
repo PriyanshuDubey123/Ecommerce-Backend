954a62a9f92edc16e56c4a22a06f24bd68e24a4f
PriyanshuDubey123 <priyanshudube397@gmail.com>
2024-01-09 23:50:22 +0530

    Testing

--- index.js
+++ index.js
@@ -72,6 +72,8 @@ opts.secretOrKey = process.env.JWT_SECRET_KEY; // TODO: should not be in code;
 //middlewares
 
 server.use(express.static(path.resolve(__dirname,'build')))
+
+
 server.use(cookieParser());
 server.use(
   session({
